3ca7d6a72ac7a1f488151706f64152ee243f015a
Henok-e <henokendeshaw21@gmail.com>
2024-02-18 03:25:30 -0800

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,14 @@
  */
 void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0 ; i < 10 ; i++)
-	{
-		_putchar(i);
-	}
-	_putchar(\n);
+i = 0;
+while (i < 10)
+{
+_putchar (i + '0');
+i++;
+}
+
+_putchar ('\n');
 }
