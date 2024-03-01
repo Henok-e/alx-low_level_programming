e2a1b36ccb7c6b51cd6009217f98fff23698db30
Henok-e <henokendeshaw21@gmail.com>
2024-03-01 03:07:36 -0800

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 /**
  * swap_int-function to  two integer pointer
  * swap value of two integers
- * @a first entry
- * @b second entry
+ * @a: first entry
+ * @b: second entry
  */
 void swap_int(int *a, int *b)
 {
