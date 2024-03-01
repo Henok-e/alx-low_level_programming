d29d2beb869ecd2eaa0e072f4bb2f7512e2de473
Henok-e <henokendeshaw21@gmail.com>
2024-03-01 01:00:08 -0800

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
- * rest_to_98 - function that takes pointer
- * updates any integer value to 98
+ * reset_to_98 - function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * @n: This is the entry
+ *
  */
 void reset_to_98(int *n)
 {
