561f0e411c16389cf93678880274e2781f7436c4
Henok-e <henokendeshaw21@gmail.com>
2024-03-01 03:36:42 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - pointer function for string
+ * return length of string
+ * strlen : string length
+ * s:string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
