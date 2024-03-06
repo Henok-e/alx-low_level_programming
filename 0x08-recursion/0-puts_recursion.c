a296719a345c3865de0f8c7e1b462280f7fbb9bf
Henok-e <henokendeshaw21@gmail.com>
2024-03-05 23:34:07 -0800

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,6 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+printf("%s\n", s);
+return;
 }
